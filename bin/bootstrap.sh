876657a7a6ab32eb8c8a285c7aa3ff00f765dae4
Guy Irvine <girvine@lic.co.nz>
2015-06-04 06:13:09 +1200

    Cleanup

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 su -c '/usr/bin/createuser -s vagrant' postgres
-su -c 'psql -c "ALTER ROLE vagrant WITH PASSWORD vagrant" template1' postgres
+su -c "psql -c \"ALTER ROLE vagrant WITH PASSWORD 'vagrant'\" template1" postgres
 echo 'cd /vagrant' >> /home/vagrant/.bashrc
 
 echo 'nameserver 10.0.2.15' > /etc/resolv.conf
@@ -14,4 +14,6 @@ cd /vagrant
 
 bundle install
 
-su -c 'rake rdb' jenkins
+rake rdb
+
+curl -X PUT -d 'pgsql://vagrant:vagrant@localhost/project' http://localhost:8500/v1/kv/project-db
