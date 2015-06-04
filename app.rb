f550046b4e2cb6ec3af0a0104b0f64f2877973b2
Guy Irvine <girvine@lic.co.nz>
2015-06-04 19:10:39 +1200

    Added update and insert

--- app.rb
+++ app.rb
@@ -37,8 +37,29 @@ get '/outcome/:id' do
   @db.queryForArray(sql, [params[:id]]).to_json
 end
 
+post '/outcome/:id' do
+  request.body.rewind
+  data = request.body.read
+  o = JSON.parse(data)
+
+  sql = 'UPDATE outcome_tbl SET name=?, description=? WHERE id = ?'
+  @db.execute(sql, [o['name'], o['description'], params[:id]])
+end
+
+post '/outcome' do
+  request.body.rewind
+  data = request.body.read
+  o = JSON.parse(data)
+
+  sql = 'INSERT INTO outcome_tbl( id, project_id, name, description ) ' \
+        "VALUES ( NEXTVAL( 'outcome_seq' ), ?, ?, ? )"
+  @db.execute(sql, [o['project_id'], o['name'], o['description']])
+
+  @db.queryForValue("SELECT CURRVAL( 'outcome_seq' )")
+end
+
 get '/project/:id/backlog' do
-  sql = 'SELECT id, name, description FROM backlog_tbl WHERE project_id = ?'
+  sql = 'SELECT id, project_id, name, description FROM backlog_tbl WHERE project_id = ?'
   @db.queryForResultset(sql, [params[:id]]).to_json
 end
 
@@ -47,14 +68,56 @@ get '/backlog/:id' do
   @db.queryForArray(sql, [params[:id]]).to_json
 end
 
+post '/backlog/:id' do
+  request.body.rewind
+  data = request.body.read
+  b = JSON.parse(data)
+
+  sql = 'UPDATE backlog_tbl SET name=?, description=? WHERE id = ?'
+  @db.execute(sql, [b['name'], b['description'], params[:id]])
+end
+
+post '/backlog' do
+  request.body.rewind
+  data = request.body.read
+  b = JSON.parse(data)
+
+  sql = 'INSERT INTO backlog_tbl( id, project_id, name, description ) ' \
+          "VALUES ( NEXTVAL( 'backlog_seq' ), ?, ?, ? )"
+  @db.execute(sql, [b['project_id'], b['name'], b['description']])
+
+  @db.queryForValue("SELECT CURRVAL( 'backlog_seq' )")
+end
+
 get '/project/:id/persona' do
   sql = 'SELECT id, name, role FROM persona_tbl WHERE project_id = ?'
   @db.queryForResultset(sql, [params[:id]]).to_json
 end
 
 get '/persona/:id' do
-  sql = 'SELECT id, name, role FROM persona_tbl WHERE id = ?'
+  sql = 'SELECT id, project_id, name, role FROM persona_tbl WHERE id = ?'
   @db.queryForArray(sql, [params[:id]]).to_json
+end
+
+post '/persona/:id' do
+  request.body.rewind
+  data = request.body.read
+  p = JSON.parse(data)
+
+  sql = 'UPDATE persona_tbl SET name=?, role=? WHERE id = ?'
+  @db.execute(sql, [p['name'], p['role'], params[:id]])
+end
+
+post '/persona' do
+  request.body.rewind
+  data = request.body.read
+  p = JSON.parse(data)
+
+  sql = 'INSERT INTO persona_tbl( id, project_id, name, role ) ' \
+          "VALUES ( NEXTVAL('persona_seq'), ?, ?, ? )"
+  @db.execute(sql, [p['project_id'], p['name'], p['role']])
+
+  @db.queryForValue("SELECT CURRVAL( 'persona_seq' )")
 end
 
 get '/project/:id/hypothesis' do
@@ -67,4 +130,25 @@ end
 get '/hypothesis/:id' do
   sql = 'SELECT id, outcome_id, description FROM hypothesis_tbl WHERE id = ?'
   @db.queryForArray(sql, [params[:id]]).to_json
+end
+
+post '/hypothesis/:id' do
+  request.body.rewind
+  data = request.body.read
+  p = JSON.parse(data)
+
+  sql = 'UPDATE hypothesis_tbl SET outcome_id=?, persona_id=?, description=? WHERE id = ?'
+  @db.execute(sql, [p['outcome_id'], p['persona_id'], p['description'], params[:id]])
+end
+
+post '/hypothesis' do
+  request.body.rewind
+  data = request.body.read
+  p = JSON.parse(data)
+
+  sql = 'INSERT INTO hypothesis_tbl( id, project_id, outcome_id, persona_id, description ) ' \
+          "VALUES ( NEXTVAL('hypothesis_seq'), ?, ?, ?, ? )"
+  @db.execute(sql, [p['project_id'], p['outcome_id'], p['persona_id'], p['description']])
+
+  @db.queryForValue("SELECT CURRVAL( 'hypothesis_seq' )")
 end
--- public/js/index.js
+++ public/js/index.js
@@ -11,65 +11,121 @@ function Project(id, n) {
   };
 }
 
-function Outcome(id, n, d) {
+function Outcome(id, p, n, d) {
   var self=this;
 
   self.id = id;
+  self.project = ko.observable(p);
   self.name=ko.observable(n);
   self.description=ko.observable(d);
 
   self.select_outcome = function() {
-    console.log( "select_outcome" );
+    $( "section" ).addClass( "hide" );
+    vm.currentOutcome( self );
+    $( "section.outcome-form" ).removeClass( "hide" );
+    $( 'section.outcome-form input' ).focus();
+  };
+
+  self.submit = function(e) {
+    $( "section" ).addClass( "hide" );
+    $( "section.outcome" ).removeClass( "hide" );
+
+    if ( self.id === null ) {
+      vm.add_outcome( self );
+    }
+
+    vm.update_outcome( self );
   };
 }
 
-function Backlog(id, n, d) {
+function Backlog(id, p, n, d) {
   var self=this;
 
   self.id = id;
+  self.project = ko.observable(p);
   self.name=ko.observable(n);
   self.description=ko.observable(d);
 
   self.select_backlog = function() {
-    console.log( "select_backlog" );
+    $( "section" ).addClass( "hide" );
+    vm.currentBacklogItem( self );
+    $( "section.backlog-form" ).removeClass( "hide" );
+    $( 'section.backlog-form input' ).focus();
+  };
+
+  self.submit = function(e) {
+    $( "section" ).addClass( "hide" );
+    $( "section.backlog" ).removeClass( "hide" );
+
+    if ( self.id === null ) {
+      vm.add_backlog( self );
+    }
+    vm.update_backlog( self );
   };
 }
 
-function Persona(id, n) {
+function Persona(id, p, n, r) {
   var self=this;
 
   self.id = id;
+  self.project = ko.observable(p);
   self.name=ko.observable(n);
+  self.role=ko.observable(r);
 
   self.select_persona = function() {
-    console.log( "select_persona" );
+    $( "section" ).addClass( "hide" );
+    vm.currentPersona( self );
+    $( "section.persona-form" ).removeClass( "hide" );
+    $( 'section.persona-form input.name' ).focus();
+  };
+
+  self.submit = function(e) {
+    $( "section" ).addClass( "hide" );
+    $( "section.persona" ).removeClass( "hide" );
+
+    if ( self.id === null ) {
+      vm.add_persona( self );
+    }
+
+    vm.update_persona( self );
   };
 }
 
-function Hypothesis(o, p, d) {
+function Hypothesis(id, pr, o, p, d) {
   var self=this;
 
+  self.id = id;
+  self.project = ko.observable(pr);
   self.outcome=ko.observable(o);
   self.persona=ko.observable(p);
   self.description=ko.observable(d);
 
-  self.click = function(e) {
+  self.select = function(e) {
     $( "section" ).addClass( "hide" );
-    $( "section.form" ).html( $( "div.hypothesis-form" ).html() );
-    $( "section.form" ).removeClass( "hide" );
-
-    $( 'section.form textarea' ).focus();
-    ko.applyBindings(self, $('section.form')[0]);
+    vm.currentHypothesis( self );
+    $( "section.hypothesis-form" ).removeClass( "hide" );
+    $( 'section.hypothesis-form textarea' ).focus();
   };
-  self.close = function(e) {
+
+  self.submit = function(e) {
     $( "section" ).addClass( "hide" );
-    ko.cleanNode($('section.form')[0]);
     $( "section.hypothesis" ).removeClass( "hide" );
+
+    if ( self.id === null ) {
+      vm.add_hypothesis( self );
+    }
+
+    vm.update_hypothesis( self );
   };
 }
 
 function HypothesisViewModel() {
   var self=this;
+
+  self.currentHypothesis = ko.observable();
+  self.currentBacklogItem = ko.observable();
+  self.currentPersona = ko.observable();
+  self.currentOutcome = ko.observable();
 
   self.project_list = ko.observableArray();
   self.project_idx = {};
@@ -91,15 +147,21 @@ function HypothesisViewModel() {
   self.hypothesis_idx = {};
   self.list = ko.observableArray();
 
-  self.addnew = function() {
-    self.show_form('hypothesis');
-    $( 'div.hypothesis textarea' ).focus();
-
-    ko.applyBindings(self, $('div.form')[0]);
+  self.new_backlogitem = function() {
+    var b = new Backlog(null, self.current_project(), "", "");
+    b.select_backlog();
   };
-
-  self.add = function(o_id, o, d) {
-    self.list.push( new Hypothesis(o_id, o, d) );
+  self.new_outcome = function() {
+    var o = new Outcome(null, self.current_project(), "", "");
+    o.select_outcome();
+  };
+  self.new_persona = function() {
+    var p = new Persona(null, self.current_project(), "", "");
+    p.select_persona();
+  };
+  self.new_hypothesis = function() {
+    var h = new Hypothesis(null, self.current_project(), null, null, null);
+    h.select();
   };
 
   self.add_project=function(p) {
@@ -133,6 +195,76 @@ function HypothesisViewModel() {
     ko.cleanNode($('div.form')[0]);
   };
 
+  self.update_outcome = function(o) {
+    data = {
+      project_id: o.project().id,
+      name: o.name(),
+      description: o.description()
+    };
+
+    if ( o.id === null ) {
+      $.post( "/outcome", JSON.stringify(data), function(id) {
+        o.id = id;
+      });
+    } else {
+      $.post( "/outcome/" + o.id, JSON.stringify(data), function() {
+      });
+    }
+  };
+
+  self.update_persona = function(p) {
+    data = {
+      project_id: p.project().id,
+      name: p.name(),
+      role: p.role()
+    };
+
+    if ( p.id === null ) {
+      $.post( "/persona", JSON.stringify(data), function(id) {
+        p.id = id;
+      });
+    } else {
+      $.post( "/persona/" + p.id, JSON.stringify(data), function() {
+      });
+    }
+  };
+
+  self.update_backlog = function(b) {
+    data = {
+      project_id: b.project().id,
+      name: b.name(),
+      description: b.description()
+    };
+
+    if ( b.id === null ) {
+      $.post( "/backlog", JSON.stringify(data), function(id) {
+        b.id=id;
+      });
+    } else {
+      $.post( "/backlog/" + b.id, JSON.stringify(data), function() {
+      });
+    }
+  };
+
+  self.update_hypothesis = function(h) {
+    data = {
+      project_id: h.project().id,
+      outcome_id: h.outcome().id,
+      persona_id: h.persona().id,
+      description: h.description()
+    };
+
+    if ( h.id === null ) {
+      $.post( "/hypothesis", JSON.stringify(data), function(id) {
+        h.id=id;
+      });
+    } else {
+      $.post( "/hypothesis/" + h.id, JSON.stringify(data), function() {
+      });
+    }
+  };
+
+
   self.get_for_project = function(id) {
     if ( vm.current_project() !== undefined ) {
       vm.current_project().selected( false );
@@ -148,17 +280,17 @@ function HypothesisViewModel() {
 
       self.outcome_list.removeAll();
       _.each(JSON.parse( outcome_data[0] ), function(el) {
-        self.add_outcome( new Outcome( el.id, el.name, el.description ) );
+        self.add_outcome( new Outcome( el.id, self.current_project(), el.name, el.description ) );
       });
 
       self.backlog_list.removeAll();
       _.each(JSON.parse( backlog_data[0] ), function(el) {
-        self.add_backlog( new Backlog( el.id, el.name, el.description ) );
+        self.add_backlog( new Backlog( el.id, self.current_project(), el.name, el.description ) );
       });
 
       self.persona_list.removeAll();
       _.each(JSON.parse( persona_data[0] ), function(el) {
-        self.add_persona(new Persona( el.id, el.name ));
+        self.add_persona(new Persona( el.id, self.current_project(), el.name, el.role ));
       });
 
       self.hypothesis_list.removeAll();
@@ -166,7 +298,7 @@ function HypothesisViewModel() {
         var o=self.outcome_idx[el.outcome_id];
         var p=self.persona_idx[el.persona_id];
 
-        self.add_hypothesis(new Hypothesis(o, p, el.description));
+        self.add_hypothesis(new Hypothesis(el.id, self.current_project(), o, p, el.description));
       });
     });
   };
